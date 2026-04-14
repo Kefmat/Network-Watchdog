dbec7a4ffd405bacd707cec858bf7b4f18b3b08a
Kefmat <kevin_mata98@hotmail.com>
2026-04-14 14:25:09 +0200

    Implementert logg-rotasjon

--- watchdog.sh
+++ watchdog.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 # Importer alle moduler
+# shellcheck source=/dev/null
 source "./modules/notifier.sh"
 source "./modules/reporter.sh"
 source "./modules/scanner.sh"
 
 # Konfigurasjon og sjekk
 WHITELIST="whitelist.txt"
+LOGFILE="logs/watchdog.log"
 [ ! -d "logs" ] && mkdir "logs"
 [ ! -f "$WHITELIST" ] && touch "$WHITELIST"
+
+# --- LOGG-VEDLIKEHOLD ---
+# Begrenser loggfilen til de siste 1000 linjene for å spare diskplass
+if [ -f "$LOGFILE" ]; then
+    tail -n 1000 "$LOGFILE" > "${LOGFILE}.tmp" && mv "${LOGFILE}.tmp" "$LOGFILE"
+fi
 
 # Håndtering av argumenter (bruker funksjonen fra scanner.sh)
 if [[ "$1" == "--add" ]]; then
@@ -22,6 +30,7 @@ sudo arp-scan --localnet | grep -E '([a-f0-9]{2}:){5}[a-f0-9]{2}' > current_scan
 TABLE_ROWS=""
 UNKNOWN_COUNT=0
 KNOWN_COUNT=0
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
 while read -r line; do
     IP=$(echo "$line" | awk '{print $1}')
@@ -35,6 +44,9 @@ while read -r line; do
         [ -z "$PORTS" ] && PORTS="Ingen åpne porter funnet"
         
         STATUS="<span style='color: #dc3545; font-weight: bold;'>UKJENT</span>"
+        
+        # Loggføring av hendelse
+        echo "[$TIMESTAMP] ALERT: Ukjent enhet detektert - IP: $IP, MAC: $MAC" >> "$LOGFILE"
         
         # Bruker send_webhook fra notifier.sh
         send_webhook "Sikkerhetsvarsel: Ukjent enhet detektert: $IP ($MAC). Produsent: $VENDOR. Porter: $PORTS"
